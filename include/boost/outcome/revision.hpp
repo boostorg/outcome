54c2e80b4ad5576cf0a23a8870df785a5a90d3ae
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-09-28 03:24:47 +0100

    First, very early, attempt at scripted conversion.

---
+++ include/boost/outcome/revision.hpp
@@ -0,0 +1,4 @@
+// Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
+#define BOOST_OUTCOME_PREVIOUS_COMMIT_REF cc8e589b543cf4fa6ca784e700187d8a84de3d59
+#define BOOST_OUTCOME_PREVIOUS_COMMIT_DATE "2017-09-28 00:56:59 +00:00"
+#define BOOST_OUTCOME_PREVIOUS_COMMIT_UNIQUE cc8e589b
---
+++ test/compile-fail/result-int-int-1.cpp
@@ -0,0 +1,14 @@
+/* clang-format off
+(error: no matching function for call to .+::result<int, int>::result|error: no matching constructor for initialization of 'result<int, int>'|cannot convert argument 1 from 'int')
+clang-format on
+*/
+
+#include "../../include/boost/outcome/result.hpp"
+
+int main()
+{
+  using namespace BOOST_OUTCOME_V2_NAMESPACE;
+  // Must not be possible to initialise a result with same R and S types
+  result<int, int> m(5);
+  return 0;
+}
